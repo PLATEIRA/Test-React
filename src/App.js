a4f1100610b8f40db2378e2ed6ea963d8e6fc1c3
Ary Plateira <ary_plateira@outlook.com>
2022-05-26 15:32:07 -0300

    Add className

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-/*import logo from './logo.svg';
-import './App.css';*/
-import React from 'react';
-import { FormularioCadastro } from './componentes/FormularioCadastro';
-import { ListaDeNotas } from './componentes/ListaDeNotas';
-function App() {
-  return (
-      <section>
+/*import logo from './logo.svg';*/
+import React, { Component } from 'react';
+import ListaDeNotas from './componentes/ListaDeNotas/ListaDeNotas';
+import FormularioCadastro from './componentes/FormularioCadastro';
+import './componentes/ListaDeNotas/App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <section className='conteudo'>
         <FormularioCadastro/>
         <ListaDeNotas/>
       </section>
-  );
+    );
+  }
+ 
+  
 }
+
 
 export default App;
